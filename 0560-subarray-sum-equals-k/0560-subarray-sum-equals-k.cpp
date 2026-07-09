d492da7714d9285ef740fc194e218fc735a5ba26
Palak <palakbansal910@gmail.com>
2026-07-09 11:05:58 +0530

    Time: 39 ms (72.49%), Space: 45.6 MB (40.91%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
+        /*Brute Force Approach
         int count = 0;
         for(int i = 0; i < n; i++){
             int currSum = 0;
@@ -9,6 +10,17 @@ public:
                 currSum += nums[j];
                 if(currSum == k) count++;
             }
+        }
+        return count;*/
+        unordered_map<int,int> map = {{0,1}};
+        int count = 0;
+        int curr_sum = 0;
+        for(int i : nums){
+            curr_sum += i;
+            if(map.count(curr_sum - k)){
+                count += map[curr_sum - k];
+            }
+            map[curr_sum]++;
         }
         return count;
     }
