06c52e523272707149a885be8c54b3b82d36cac8
Palak <palakbansal910@gmail.com>
2026-07-13 13:15:49 +0530

    Time: 3 ms (90%), Space: 10.8 MB (78.25%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,8 +1,37 @@
 class Solution {
 public:
+    int& minVal(int &a, int &b, int &c) {
+            if (a <= b && a <= c)
+                return a;
+            else if (b <= a && b <= c)
+                return b;
+            else
+                return c;
+    }
     int maximumLength(string s) {
         int n = s.size();
-        unordered_map<string,int> freq;
+        vector<vector<int>> map(26,vector<int>(3,-1));
+        int maxlen = -1;
+        char prev = '*';
+        int curr_len = 1;
+        for(char ch : s){
+            int ind = ch -'a';
+            curr_len = (prev == ch)? curr_len+1:1;
+            prev = ch;
+            int& mini = minVal(map[ind][0],map[ind][1],map[ind][2]);
+            if(curr_len > mini){
+                mini = curr_len;
+            }        
+        }
+        for(int i = 0; i < 26; i++){
+            int temp = min({map[i][0],map[i][1],map[i][2]});
+            maxlen = max(maxlen, temp);
+        }
+        return maxlen;
+
+        
+    }
+    /*unordered_map<string,int> freq;
 
         for(int i = 0; i < n; i++){
             string sub = "";
@@ -31,6 +60,6 @@ public:
         }
 
         return ans;
+        */
         
-    }
 };
