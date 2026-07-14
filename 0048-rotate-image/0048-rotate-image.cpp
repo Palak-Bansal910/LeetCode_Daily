1e6570eaf9cba41302967d7dd054d5149377aadb
Palak <palakbansal910@gmail.com>
2026-07-14 11:49:06 +0530

    Time: 0 ms (100%), Space: 10.2 MB (70.39%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,7 +4,7 @@ public:
         int n = matrix.size();
         //vector<vector<int>> temp(n,vector<int>(n,0));
         for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
+            for(int j = i; j < n; j++){
                swap(matrix[i][j], matrix[j][i]);
             }
         }
