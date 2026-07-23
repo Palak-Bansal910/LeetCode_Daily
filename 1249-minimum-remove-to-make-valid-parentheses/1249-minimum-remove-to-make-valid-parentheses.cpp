520dc126b52b8a683d8dd3411bc85c7adbf09be5
Palak <palakbansal910@gmail.com>
2026-07-23 11:08:31 +0530

    Time: 8 ms (57.2%), Space: 16.9 MB (5.35%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,33 +1,54 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<int> st;
-
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == '(') {
-                st.push(i);
+        int bal = 0;
+        string ans = "";
+        stack<char> st;
+        for(char ch : s){
+            if(ch == '(') bal++;
+            else if(ch == ')'){
+                if(bal == 0) continue;
+                bal--;
             }
-            else if(s[i] == ')') {
-                if(!st.empty()) {
-                    st.pop();
-                }
-                else {
-                    s[i] = '*';  
-                }
-            }
+            st.push(ch);
         }
-
-        while(!st.empty()) {
-            s[st.top()] = '*';  
-            st.pop();
+        string temp = "";
+        for(stack<char> i = st;!i.empty();i.pop()){
+            temp += i.top();
         }
-
-        string ans;
-        for(char ch : s) {
-            if(ch != '*')
-                ans += ch;
+        for(int i = 0; i < temp.size(); i++){
+            if(temp[i] == '(' && bal-- > 0) continue;
+            ans += temp[i];
         }
-
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
+        //  stack<int> st;
+
+        // for(int i = 0; i < s.size(); i++) {
+        //     if(s[i] == '(') {
+        //         st.push(i);
+        //     }
+        //     else if(s[i] == ')') {
+        //         if(!st.empty()) {
+        //             st.pop();
+        //         }
+        //         else {
+        //             s[i] = '*';  
+        //         }
+        //     }
+        // }
+
+        // while(!st.empty()) {
+        //     s[st.top()] = '*';  
+        //     st.pop();
+        // }
+
+        // string ans;
+        // for(char ch : s) {
+        //     if(ch != '*')
+        //         ans += ch;
+        // }
+
+        // return ans;
