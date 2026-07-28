d1bb53483fb970849ffa16642ffce5bd7eb4193f
Palak <palakbansal910@gmail.com>
2026-07-28 11:54:43 +0530

    Time: 0 ms (100%), Space: 16.6 MB (99.21%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,42 +11,46 @@
  */
 class Solution {
 public:
-    // int maxDepth(TreeNode* root) {
-    //     if(root == NULL) return 0;
-    //     int left = maxDepth(root->left);
-    //     int right = maxDepth(root->right);
-    //     return max(left,right) + 1;
-    // }
-    // vector<vector<int>> solve(TreeNode* root, int currLevel){
-    //     if(root == NULL) return {};
-    //     int h = maxDepth(root);
-    //     vector<vector<int>> result(h);
-    //     solve(root->left, currLevel+ 1);
-    //     result[curr].push_back(root->val);
-    //     solve(root->right, currLevel+ 1);
-    //     return result;
-    // }
+    //*DFS APPROACH(RECURSION)*
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return max(left,right) + 1;
+    }
+
+    void solve(TreeNode* root, int currLevel,vector<vector<int>>& result){
+        if(root == NULL) return;
+        solve(root->left, currLevel+ 1,result);
+        result[currLevel].push_back(root->val);
+        solve(root->right, currLevel+ 1,result);
+    }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if(root == NULL) return result;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            int n = q.size();
-            vector<int> temp;
-            for(int i = 0; i < n; i++){
-                TreeNode* curr = q.front();
-                q.pop();
-                temp.push_back(curr->val);
-                if(curr->left){
-                    q.push(curr->left);
-                }
-                if(curr->right){
-                    q.push(curr->right);
-                }
-            }
-            result.push_back(temp);
-        }
+        int h = maxDepth(root);
+        vector<vector<int>> result(h,vector<int>(0));
+        solve(root,0,result);
         return result;
     }
 };
+// // *BFS Approach(ITERATIVE)*
+        // vector<vector<int>> result;
+        // if(root == NULL) return result;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int n = q.size();
+        //     vector<int> temp;
+        //     for(int i = 0; i < n; i++){
+        //         TreeNode* curr = q.front();
+        //         q.pop();
+        //         temp.push_back(curr->val);
+        //         if(curr->left){
+        //             q.push(curr->left);
+        //         }
+        //         if(curr->right){
+        //             q.push(curr->right);
+        //         }
+        //     }
+        //     result.push_back(temp);
+        // }
+        // return result;
