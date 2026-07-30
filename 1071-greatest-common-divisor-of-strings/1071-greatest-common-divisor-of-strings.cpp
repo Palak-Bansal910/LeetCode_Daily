980ae82052cc551a12fb7bf29d381e946f388ef6
Palak <palakbansal910@gmail.com>
2026-07-30 11:26:26 +0530

    Time: 0 ms (100%), Space: 11.3 MB (10.91%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+         if(str1.size() < str2.size()){
+            return gcdOfStrings(str2,str1);
+        }
+        if(str1.find(str2) != 0){
+            return "";
+        }
+        if(str2.empty()){
+            return str1;
+        }
+
+        return gcdOfStrings(str1.substr(str2.size()),str2);
+    }
+};
