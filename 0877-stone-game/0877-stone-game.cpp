41e7d9ee4ecf3af245db1efca6f949fafea65a4c
Palak <palakbansal910@gmail.com>
2026-08-02 13:00:01 +0530

    Time: 0 ms (100%), Space: 10.5 MB (46.05%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // We have returned true because the no of piles is even and no of stones in every pile is odd, so starting with Alice, in however way she pick she will only end up getting more stones, hence the answer will always be true.
+    // The other solution for this problem that shows our understanding is through DP Memoization.
     // int solve(vector<int>& piles,int l,int r){
     //     if(l == r) return piles[l];
     //     int left = piles[l] - solve(piles,l+1,r);
